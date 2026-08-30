8180d6375a1a9be0bb09aa8e888c592b568f11c4
luisjrn <luiscarlossantossilvajunior@gmail.com>
2026-08-30 11:01:11 -0300

    caneta

--- Caneta.java
+++ Caneta.java
@@ -1,10 +1,10 @@
 public class Caneta {
-	private String modelo, cor;
-	private float ponta;
-	private int carga;
+	public String modelo, cor;
+	private double ponta;
+	protected int carga;
 	private boolean tampa;
 	
-	public Caneta(String modelo, String cor, float ponta, int carga){
+	public Caneta(String modelo, String cor, double ponta, int carga){
 		this.modelo = modelo;
 		this.cor = cor;
 		this.ponta = ponta;
@@ -13,8 +13,8 @@ public class Caneta {
 	}
 	
 	public void status(){
-		System.out.println("Uma caneta " + this.cor +
-		"\ntampada? " + this.tampa + "\nModelo: " + this.modelo
+		System.out.println("Uma caneta " + this.cor + 
+		(this.tampa ? "Tampada." : "Destampada.") + "\nModelo: " + this.modelo
 		+ "\nPonta: " + this.ponta + "\nCarga: " + this.carga);
 	}
 	
@@ -25,11 +25,11 @@ public class Caneta {
 			System.out.println("Rabiscando.");
 	}
 	
-	public void tampar(){
+	protected void tampar(){
 		this.tampa = true;
 	}
 
-	public void destampar(){
+	protected void destampar(){
 		this.tampa = false;
 	}
 	
